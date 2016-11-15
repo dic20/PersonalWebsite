3654112955b9a682d74ecd4ea17dd3288ee8a240
Daniel Critchfield <dic20@pitt.edu>
2016-11-15 10:10:12 -0500

    Updates from personalwebsite2

---
+++ PersonalWebsite2/JS/LandingPage.js
@@ -0,0 +1,9 @@
+function SignInUser() {
+	//need to put in some validation for sign in like checking a database
+	window.location.href="../PHP/index.php";
+}
+
+function NewUser() {
+	window.location.href="../PHP/newUser.php";
+}
+
